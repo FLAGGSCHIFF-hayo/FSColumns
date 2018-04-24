8119c8aa62c5ede990403f2e6a3701435e4ade69
Adrian Damerow <adrian.damerow@flaggschiff.de>
2018-04-24 08:43:15 +0200

    Applied columns logic from tl_content to tl_form_field

--- src/Resources/contao/dca/tl_form_field.php
+++ src/Resources/contao/dca/tl_form_field.php
@@ -1,6 +1,7 @@
 <?php
 
 $GLOBALS['TL_DCA']['tl_form_field']['config']['onload_callback'][] = array('tl_form_field_fs_columns','addFSColumnsDCA');
+$GLOBALS['TL_DCA']['tl_form_field']['config']['onsubmit_callback'][] = array('tl_form_field_fs_columns','removeColValues');
 
 $fscoloptions = array('full', 'half', 'third', 'quarter', 'two-third', 'three-quarter', 'hidden');
 
@@ -8,135 +9,102 @@ if($GLOBALS['TL_CONFIG']['fscol_sizes']=='all'){
     $fscoloptions = array(12,11,10,9,8,7,6,5,4,3,2,1,'hidden');
 }
 
+$GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_columns'] = array(
+    'exclude' => false,
+    'label' => &$GLOBALS['TL_LANG']['tl_form_field']['fs_columns'],
+    'inputType' => 'select',
+    'options' => $fscoloptions,
+    'eval' => array('submitOnChange' => true, 'tl_class'=>'w50 clr'),
+    'reference' => &$GLOBALS['TL_LANG']['tl_form_field'],
+    'sql' => "varchar(32) NOT NULL default ''"
+);
+if($GLOBALS['TL_CONFIG']['fscol_sizes']=='all') {
+    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_columns']['eval']['submitOnChange'] = false;
+}
+$GLOBALS['TL_DCA']['tl_form_field']['palettes']['__selector__'][] = 'fs_responsive';
+
+$GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_responsive'] = array
+(
+    'label' => &$GLOBALS['TL_LANG']['tl_form_field']['fs_responsive'],
+    'exclude' => false,
+    'inputType' => 'checkbox',
+    'eval' => array('submitOnChange' => true, 'tl_class'=>'w50 clr'),
+    'sql' => "char(1) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_xxs_columns'] = array(
+    'exclude' => false,
+    'label' => &$GLOBALS['TL_LANG']['tl_form_field']['fs_xxs_columns'],
+    'inputType' => 'select',
+    'options' => $fscoloptions,
+    'reference' => &$GLOBALS['TL_LANG']['tl_form_field'],
+    'eval' => array('tl_class'=>'w50 clr'),
+    'sql' => "varchar(32) NOT NULL default ''"
+);
+if($GLOBALS['TL_CONFIG']['fscol_sizes']!='all') {
+    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_xxs_columns']['load_callback'] = array(array('tl_form_field_fs_columns', 'loadXXSColumns'));
+}
+
+$GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_xs_columns'] = array(
+    'exclude' => false,
+    'label' => &$GLOBALS['TL_LANG']['tl_form_field']['fs_xs_columns'],
+    'inputType' => 'select',
+    'options' => $fscoloptions,
+    'reference' => &$GLOBALS['TL_LANG']['tl_form_field'],
+    'eval' => array('tl_class'=>'w50 clr'),
+    'sql' => "varchar(32) NOT NULL default ''"
+);
+if($GLOBALS['TL_CONFIG']['fscol_sizes']!='all') {
+    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_xs_columns']['load_callback'] = array(array('tl_form_field_fs_columns', 'loadXSColumns'));
+}
+
+$GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_sm_columns'] = array(
+    'exclude' => false,
+    'label' => &$GLOBALS['TL_LANG']['tl_form_field']['fs_sm_columns'],
+    'inputType' => 'select',
+    'options' => $fscoloptions,
+    'reference' => &$GLOBALS['TL_LANG']['tl_form_field'],
+    'eval' => array('tl_class'=>'w50 clr'),
+    'sql' => "varchar(32) NOT NULL default ''"
+);
+if($GLOBALS['TL_CONFIG']['fscol_sizes']!='all') {
+    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_sm_columns']['load_callback'] = array(array('tl_form_field_fs_columns', 'loadSMColumns'));
+}
+
+$GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_md_columns'] = array(
+    'exclude' => false,
+    'label' => &$GLOBALS['TL_LANG']['tl_form_field']['fs_md_columns'],
+    'inputType' => 'select',
+    'options' => $fscoloptions,
+    'reference' => &$GLOBALS['TL_LANG']['tl_form_field'],
+    'eval' => array('tl_class'=>'w50 clr'),
+    'sql' => "varchar(32) NOT NULL default ''"
+);
+if($GLOBALS['TL_CONFIG']['fscol_sizes']!='all') {
+    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_md_columns']['load_callback'] = array(array('tl_form_field_fs_columns', 'loadMDColumns'));
+}
+
+$GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_lg_columns'] = array(
+    'exclude' => false,
+    'label' => &$GLOBALS['TL_LANG']['tl_form_field']['fs_lg_columns'],
+    'inputType' => 'select',
+    'options' => $fscoloptions,
+    'reference' => &$GLOBALS['TL_LANG']['tl_form_field'],
+    'eval' => array('tl_class'=>'w50 clr'),
+    'sql' => "varchar(32) NOT NULL default ''"
+);
+if($GLOBALS['TL_CONFIG']['fscol_sizes']!='all') {
+    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_lg_columns']['load_callback'] = array(array('tl_form_field_fs_columns', 'loadLGColumns'));
+}
+
 if($GLOBALS['TL_CONFIG']['bootstrap']=='regular'||!$GLOBALS['TL_CONFIG']['bootstrap']){
-    $GLOBALS['TL_DCA']['tl_form_field']['subpalettes']['fs_responsive'] = 'fs_xs_columns,fs_sm_columns,fs_md_columns';
-
-    $GLOBALS['TL_DCA']['tl_form_field']['palettes']['__selector__'][] = 'fs_responsive';
-
-    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_columns'] = array(
-        'exclude'                 => false,
-        'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['fs_columns'],
-        'inputType'               => 'select',
-        'options'                 => $fscoloptions,
-        'eval'                    => array('tl_class'=>'w50 clr'),
-        'reference'               => &$GLOBALS['TL_LANG']['tl_form_field'],
-        'sql'                     => "varchar(32) NOT NULL default ''"
-    );
-
-    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_responsive'] = array
-    (
-        'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['fs_responsive'],
-        'exclude'                 => false,
-        'inputType'               => 'checkbox',
-        'eval'                    => array('tl_class'=>'w50 clr','submitOnChange'=>true),
-        'sql'                     => "char(1) NOT NULL default ''"
-    );
-
-    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_xs_columns'] = array(
-        'exclude'                 => false,
-        'search'                  => true,
-        'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['fs_xs_columns'],
-        'inputType'               => 'select',
-        'options'                 => $fscoloptions,
-        'reference'               => &$GLOBALS['TL_LANG']['tl_form_field'],
-        'sql'                     => "varchar(32) NOT NULL default ''"
-    );
-
-    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_sm_columns'] = array(
-        'exclude'                 => false,
-        'search'                  => true,
-        'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['fs_sm_columns'],
-        'inputType'               => 'select',
-        'options'                 => $fscoloptions,
-        'reference'               => &$GLOBALS['TL_LANG']['tl_form_field'],
-        'sql'                     => "varchar(32) NOT NULL default ''"
-    );
-
-    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_md_columns'] = array(
-        'exclude'                 => false,
-        'search'                  => true,
-        'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['fs_md_columns'],
-        'inputType'               => 'select',
-        'options'                 => $fscoloptions,
-        'reference'               => &$GLOBALS['TL_LANG']['tl_form_field'],
-        'sql'                     => "varchar(32) NOT NULL default ''"
-    );
+    $GLOBALS['TL_DCA']['tl_form_field']['subpalettes']['fs_responsive'] = 'fs_md_columns,fs_sm_columns,fs_xs_columns';
+    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_xs_columns']['label'] = &$GLOBALS['TL_LANG']['tl_form_field']['fs_xs_columns2'];
+    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_md_columns']['label'] = &$GLOBALS['TL_LANG']['tl_form_field']['fs_md_columns2'];
 }
 else{
-    $GLOBALS['TL_DCA']['tl_form_field']['subpalettes']['fs_responsive'] = 'fs_xxs_columns,fs_xs_columns,fs_sm_columns,fs_md_columns,fs_lg_columns';
-
-    $GLOBALS['TL_DCA']['tl_form_field']['palettes']['__selector__'][] = 'fs_responsive';
-
-    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_columns'] = array(
-        'exclude' => false,
-        'label' => &$GLOBALS['TL_LANG']['tl_form_field']['fs_columns'],
-        'inputType' => 'select',
-        'options' => $fscoloptions,
-        'eval' => array('tl_class'=>'w50 clr'),
-        'reference' => &$GLOBALS['TL_LANG']['tl_form_field'],
-        'sql' => "varchar(32) NOT NULL default ''"
-    );
-
-    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_responsive'] = array
-    (
-        'label' => &$GLOBALS['TL_LANG']['tl_form_field']['fs_responsive'],
-        'exclude' => false,
-        'inputType' => 'checkbox',
-        'eval'                    => array('tl_class'=>'w50 clr','submitOnChange' => true),
-        'sql' => "char(1) NOT NULL default ''"
-    );
-
-    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_xxs_columns'] = array(
-        'exclude' => false,
-        'search' => true,
-        'label' => &$GLOBALS['TL_LANG']['tl_form_field']['fs_xxs_columns'],
-        'inputType' => 'select',
-        'options' => $fscoloptions,
-        'reference' => &$GLOBALS['TL_LANG']['tl_form_field'],
-        'sql' => "varchar(32) NOT NULL default ''"
-    );
-
-    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_xs_columns'] = array(
-        'exclude' => false,
-        'search' => true,
-        'label' => &$GLOBALS['TL_LANG']['tl_form_field']['fs_xs_columns'],
-        'inputType' => 'select',
-        'options' => $fscoloptions,
-        'reference' => &$GLOBALS['TL_LANG']['tl_form_field'],
-        'sql' => "varchar(32) NOT NULL default ''"
-    );
-
-    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_sm_columns'] = array(
-        'exclude' => false,
-        'search' => true,
-        'label' => &$GLOBALS['TL_LANG']['tl_form_field']['fs_sm_columns'],
-        'inputType' => 'select',
-        'options' => $fscoloptions,
-        'reference' => &$GLOBALS['TL_LANG']['tl_form_field'],
-        'sql' => "varchar(32) NOT NULL default ''"
-    );
-
-    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_md_columns'] = array(
-        'exclude' => false,
-        'search' => true,
-        'label' => &$GLOBALS['TL_LANG']['tl_form_field']['fs_md_columns'],
-        'inputType' => 'select',
-        'options' => $fscoloptions,
-        'reference' => &$GLOBALS['TL_LANG']['tl_form_field'],
-        'sql' => "varchar(32) NOT NULL default ''"
-    );
-
-    $GLOBALS['TL_DCA']['tl_form_field']['fields']['fs_lg_columns'] = array(
-        'exclude' => false,
-        'search' => true,
-        'label' => &$GLOBALS['TL_LANG']['tl_form_field']['fs_lg_columns'],
-        'inputType' => 'select',
-        'options' => $fscoloptions,
-        'reference' => &$GLOBALS['TL_LANG']['tl_form_field'],
-        'sql' => "varchar(32) NOT NULL default ''"
-    );
+    $GLOBALS['TL_DCA']['tl_form_field']['subpalettes']['fs_responsive'] = 'fs_lg_columns,fs_md_columns,fs_sm_columns,fs_xs_columns,fs_xxs_columns';
 }
-
 
 class tl_form_field_fs_columns extends \Backend
 {
@@ -146,12 +114,215 @@ class tl_form_field_fs_columns extends \Backend
         parent::__construct();
     }
 
-    public function addFSColumnsDCA(){
+    public function loadLGColumns($val,$dc){
+        $return = $val;
+        if(!$val && $GLOBALS['TL_CONFIG']['fscol_sizes']!='all'){
+            $fs_columns = $dc->fs_columns;
+            if(!$fs_columns){
+                $fs_columns = $dc->activeRecord->fs_columns;
+            }
+            switch($fs_columns){
+                case 'full':
+                    $return = 'full';
+                    break;
+                case 'half':
+                    $return = 'half';
+                    break;
+                case 'third':
+                    $return = 'third';
+                    break;
+                case 'quarter':
+                    $return = 'quarter';
+                    break;
+                case 'two-third':
+                    $return = 'two-third';
+                    break;
+                case 'three-quarter':
+                    $return = 'three-quarter';
+                    break;
+                case 'hidden':
+                    $return = 'hidden';
+                    break;
+            }
+            if($return!=$val) {
+                $this->Database->prepare("UPDATE tl_form_field SET fs_lg_columns=? WHERE id=?")->execute($return, $dc->id);
+            }
+        }
+        return $return;
+    }
 
-        foreach($GLOBALS['TL_DCA']['tl_form_field']['palettes'] as &$GPalette){
-            $GPalette = str_replace(',type,', ',type,fs_columns,fs_responsive,', $GPalette);
-            $GPalette = str_replace(',type;', ',type,fs_columns,fs_responsive;', $GPalette);
-            $GPalette = str_replace('fs_columns,fs_responsive,fs_columns,fs_responsive', 'fs_columns,fs_responsive', $GPalette);
+    public function loadMDColumns($val,$dc){
+        $return = $val;
+        if(!$val && $GLOBALS['TL_CONFIG']['fscol_sizes']!='all'){
+            $fs_columns = $dc->fs_columns;
+            if(!$fs_columns){
+                $fs_columns = $dc->activeRecord->fs_columns;
+            }
+            switch($fs_columns){
+                case 'full':
+                    $return = 'full';
+                    break;
+                case 'half':
+                    $return = 'half';
+                    break;
+                case 'third':
+                    $return = 'third';
+                    break;
+                case 'quarter':
+                    $return = 'quarter';
+                    break;
+                case 'two-third':
+                    $return = 'two-third';
+                    break;
+                case 'three-quarter':
+                    $return = 'three-quarter';
+                    break;
+                case 'hidden':
+                    $return = 'hidden';
+                    break;
+            }
+            if($return!=$val) {
+                $this->Database->prepare("UPDATE tl_form_field SET fs_md_columns=? WHERE id=?")->execute($return, $dc->id);
+            }
+        }
+        return $return;
+    }
+
+    public function loadSMColumns($val,$dc){
+        $return = $val;
+        if(!$val && $GLOBALS['TL_CONFIG']['fscol_sizes']!='all'){
+            $fs_columns = $dc->fs_columns;
+            if(!$fs_columns){
+                $fs_columns = $dc->activeRecord->fs_columns;
+            }
+            switch($fs_columns){
+                case 'full':
+                    $return = 'full';
+                    break;
+                case 'half':
+                    $return = 'half';
+                    break;
+                case 'third':
+                    $return = 'half';
+                    break;
+                case 'quarter':
+                    $return = 'half';
+                    break;
+                case 'two-third':
+                    $return = 'half';
+                    break;
+                case 'three-quarter':
+                    $return = 'half';
+                    break;
+                case 'hidden':
+                    $return = 'hidden';
+                    break;
+            }
+            if($return!=$val) {
+                $this->Database->prepare("UPDATE tl_form_field SET fs_sm_columns=? WHERE id=?")->execute($return, $dc->id);
+            }
+        }
+        return $return;
+    }
+
+    public function loadXSColumns($val,$dc){
+        $return = $val;
+        if(!$val && $GLOBALS['TL_CONFIG']['fscol_sizes']!='all'){
+            $fs_columns = $dc->fs_columns;
+            if(!$fs_columns){
+                $fs_columns = $dc->activeRecord->fs_columns;
+            }
+            switch($fs_columns){
+                case 'full':
+                    $return = 'full';
+                    break;
+                case 'half':
+                    $return = 'full';
+                    break;
+                case 'third':
+                    $return = 'full';
+                    break;
+                case 'quarter':
+                    $return = 'full';
+                    break;
+                case 'two-third':
+                    $return = 'full';
+                    break;
+                case 'three-quarter':
+                    $return = 'full';
+                    break;
+                case 'hidden':
+                    $return = 'hidden';
+                    break;
+            }
+            if($return!=$val) {
+                $this->Database->prepare("UPDATE tl_form_field SET fs_xs_columns=? WHERE id=?")->execute($return, $dc->id);
+            }
+        }
+        return $return;
+    }
+
+    public function loadXXSColumns($val,$dc){
+        $return = $val;
+        if(!$val && $GLOBALS['TL_CONFIG']['fscol_sizes']!='all'){
+            $fs_columns = $dc->fs_columns;
+            if(!$fs_columns){
+                $fs_columns = $dc->activeRecord->fs_columns;
+            }
+            switch($fs_columns){
+                case 'full':
+                    $return = 'full';
+                    break;
+                case 'half':
+                    $return = 'full';
+                    break;
+                case 'third':
+                    $return = 'full';
+                    break;
+                case 'quarter':
+                    $return = 'full';
+                    break;
+                case 'two-third':
+                    $return = 'full';
+                    break;
+                case 'three-quarter':
+                    $return = 'full';
+                    break;
+                case 'hidden':
+                    $return = 'hidden';
+                    break;
+            }
+            if($return!=$val) {
+                $this->Database->prepare("UPDATE tl_form_field SET fs_xxs_columns=? WHERE id=?")->execute($return, $dc->id);
+            }
+        }
+        return $return;
+    }
+
+    public function removeColValues($dc){
+        if($GLOBALS['TL_CONFIG']['fscol_sizes']!='all') {
+            $fs_responsive = $dc->activeRecord->fs_responsive;
+            if (!$fs_responsive) {
+                $this->Database->prepare("UPDATE tl_form_field SET fs_xxs_columns='', fs_xs_columns='', fs_sm_columns='', fs_md_columns='', fs_lg_columns='' WHERE id=?")->execute($dc->id);
+            }
+        }
+    }
+
+    public function addFSColumnsDCA()
+    {
+        foreach ($GLOBALS['TL_DCA']['tl_form_field']['palettes'] as &$GPalette) {
+            if (!is_array($GPalette)) {
+
+                if (strpos($GPalette, ',nofscolumns')) {
+                    $GPalette = str_replace(',nofscolumns', '', $GPalette);
+                } else {
+                    $GPalette = str_replace(',type,', ',type,fs_columns,fs_responsive,', $GPalette);
+                    $GPalette = str_replace(',type;', ',type,fs_columns,fs_responsive;', $GPalette);
+                    if($GLOBALS['TL_CONFIG']['fscol_sizes']=='all'){
+                        $GPalette = str_replace('fs_columns,fs_responsive,fs_columns,fs_responsive', 'fs_columns,fs_responsive', $GPalette);
+                    }
+                }
+            }
         }
 
     }
--- src/Resources/contao/languages/de/tl_form_field.php
+++ src/Resources/contao/languages/de/tl_form_field.php
@@ -3,9 +3,13 @@
 
 $GLOBALS['TL_LANG']['tl_form_field']['fs_columns'] = array('Breite des Elements','Bitte geben Sie die Breite des Elements an.');
 $GLOBALS['TL_LANG']['tl_form_field']['fs_responsive'] = array('Responsive Größe','Möchten Sie die Breite für kleinere Gerätegrößen individuell anpassen?');
-$GLOBALS['TL_LANG']['tl_form_field']['fs_xs_columns'] = array('Bei sehr kleinen Geräten ( Mobiltelefone )','Bitte geben Sie die Breite des Elements an.');
-$GLOBALS['TL_LANG']['tl_form_field']['fs_sm_columns'] = array('Bei kleinen Geräten ( Tablets )','Bitte geben Sie die Breite des Elements an.');
-$GLOBALS['TL_LANG']['tl_form_field']['fs_md_columns'] = array('Bei mittelgroßen Geräten ( Computer )','Bitte geben Sie die Breite des Elements an.');
+$GLOBALS['TL_LANG']['tl_form_field']['fs_xxs_columns'] = array('Bei den kleinsten Geräten','Bitte geben Sie die Breite des Elements an.');
+$GLOBALS['TL_LANG']['tl_form_field']['fs_xs_columns'] = array('Bei sehr kleinen Geräten ( ab 480px Breite )','Bitte geben Sie die Breite des Elements an.');
+$GLOBALS['TL_LANG']['tl_form_field']['fs_xs_columns2'] = array('Bei sehr kleinen Geräten','Bitte geben Sie die Breite des Elements an.');
+$GLOBALS['TL_LANG']['tl_form_field']['fs_sm_columns'] = array('Bei kleinen Geräten ( ab 768px Breite )','Bitte geben Sie die Breite des Elements an.');
+$GLOBALS['TL_LANG']['tl_form_field']['fs_md_columns'] = array('Bei mittelgroßen Geräten ( ab 992px Breite )','Bitte geben Sie die Breite des Elements an.');
+$GLOBALS['TL_LANG']['tl_form_field']['fs_md_columns2'] = array('Bei mittelgroßen Geräten ( ab 992px bis 1200px Breite )','Bitte geben Sie die Breite des Elements an.');
+$GLOBALS['TL_LANG']['tl_form_field']['fs_lg_columns'] = array('Bei großen Geräten ( ab 1200px bis 1680px Breite )','Bitte geben Sie die Breite des Elements an.');
 $GLOBALS['TL_LANG']['tl_form_field']['full'] = 'Vollspaltig';
 $GLOBALS['TL_LANG']['tl_form_field']['half'] = 'Halbspaltig';
 $GLOBALS['TL_LANG']['tl_form_field']['third'] = 'Drittespaltig';
